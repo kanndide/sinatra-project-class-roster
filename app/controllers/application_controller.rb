4d9e1ce6088132c903457eeefda1e56f47eb6eec
kanndide <kanndide@gmail.com>
2018-06-16 10:30:58 +0300

    working on application controller routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,65 @@
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
-  set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
+  enable :sessions
+  set :session_secret, "my_application_secret"
 
   get '/' do
     erb :index
+  end
+
+  get '/signup' do 
+  	if logged_in?
+  		redirect '/'
+  	else
+		  erb :create_user
+	end
+  end
+
+  post "/signup" do
+    @user = User.create(params)
+  
+    if @user.save && params[:username] != "" && params[:password] != ""
+      session[:user_id] = @user.id
+      redirect '/tweets'
+    else
+      redirect '/signup'
+    end
+  end
+
+  get '/login' do
+  	if logged_in?
+    	redirect '/tweets'
+    else
+  		erb :'/users/login'
+  	end
+  end
+
+  post "/login" do
+    user = User.find_by(:username => params[:username])
+    
+    if user && user.authenticate(params[:password]) && params[:username] != "" && params[:password] != ""
+        session[:user_id] = user.id
+        redirect "/tweets"
+    else
+        redirect "/failure"
+    end
+
+  end
+
+  get '/logout' do
+  	session.clear
+    redirect "/login"
+  end
+
+  helpers do
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
   end
 
 end
